82698044bedf497b7e50ad2f45a8d2d5f7be6f03
larahy <larahy@gmail.com>
2014-01-11 14:15:14 +0000

    tags and filters added

--- server.rb
+++ server.rb
@@ -6,6 +6,7 @@ env = ENV["RACK_ENV"] || "development"
 DataMapper.setup(:default, "postgres://localhost/bookmarks_#{env}")
 
 require './lib/link'
+require './lib/tag'
 
 DataMapper.finalize
 
@@ -19,6 +20,15 @@ end
 post '/links' do
   url = params["url"]
   title = params["title"]
-  Link.create(:url => url, :title => title)
+  tags = params["tags"].split(" ").map do |tag|
+    Tag.first_or_create(:text => tag)
+  end
+  Link.create(:url => url, :title => title, :tags => tags)
   redirect to('/')
 end
+
+get '/tags/:text' do 
+  tag = Tag.first(:text => params[:text])
+  @links = tag ? tag.links : []
+  erb :index 
+end 
