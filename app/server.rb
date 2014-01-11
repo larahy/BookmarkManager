76ac3d9c0e6992251dae71e8675fdbbf9076f8e1
larahy <larahy@gmail.com>
2014-01-11 18:29:56 +0000

    input error recovery

--- app/server.rb
+++ app/server.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'data_mapper'
+require 'rack-flash'
 require './lib/link'
 require './lib/tag'
 require './lib/user'
@@ -7,7 +8,8 @@ require_relative 'helpers/application'
 require_relative 'datamapper_setup'
 
 enable :sessions
-set :session_secret, 'my unique encryption key!'
+set :session_secret, 'unique'
+use Rack::Flash 
 
 
 get '/' do
@@ -30,14 +32,21 @@ get '/tags/:text' do
 end
 
 get '/users/new' do
+  @user = User.new
   erb :"users/new"
 end
 
 post '/users' do
-  user = User.create(:email => params[:email], 
+  @user = User.new(:email => params[:email], 
               :password => params[:password], 
               :password_confirmation => params[:password_confirmation])  
-  session[:user_id] = user.id
+  if @user.save
+    session[:user_id] = @user.id
   redirect to('/')
+  else
+    flash[:notice] = "These passwords do not match"
+  erb :"users/new"  
+  end
+
 end
 
