4948b43b7a6ca47c0b3aff4c550bfeba69beb366
larahy <larahy@gmail.com>
2014-01-11 16:42:27 +0000

    password conifirmation capability added

--- lib/user.rb
+++ lib/user.rb
@@ -7,12 +7,14 @@ class User
   property :email, String
   property :password_digest, Text
 
-  def password=(password)
-    self.password_digest = BCrypt::Password.create(password)
-  end
+  attr_reader :password 
+  attr_accessor :password_confirmation
 
-  def password
-    BCrypt::Password.new(password_digest)
+  validates_confirmation_of :password 
+
+  def password=(password)
+    @password = password 
+    self.password_digest = BCrypt::Password.create(password)
   end
   
 end
--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -8,10 +8,15 @@ feature 'user signs up' do
     expect(User.first.email).to eq("example@example.com")
   end
 
-  def sign_up(email = "example@example.com", password = "password")
+  scenario 'with a non-matching password' do 
+    lambda { sign_up('larahy@gmail.com', 'pword', "password") }.should change(User, :count).by(0)
+  end
+
+  def sign_up(email = "example@example.com", password = "password", password_confirmation = "password")
     visit '/users/new'
     fill_in :email, :with => email
     fill_in :password, :with => password
+    fill_in :password_confirmation, :with => password_confirmation
     click_button "Sign up"
   end
 
